974f0e17e9a2d587c4945c658c15dd3cc8a44e4a
Quentin DUPERON <qduperon@student.42.fr>
2019-09-18 17:49:54 +0200

    bonus1

---
+++ Bonus1/source.c
@@ -0,0 +1,15 @@
+
+int main(int argc, char const **argv)
+{
+  int nb;
+  char buf[56];
+
+  nb = atoi(argv[1]);
+
+  if (nb > 9)
+    return (0);
+  memcpy(buf, argv[2], nb);
+  if (false)
+    execl("/bin/sh", NULL, NULL);
+  return(0);
+}
